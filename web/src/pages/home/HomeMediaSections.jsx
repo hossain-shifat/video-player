c5c076c6aa3c22126556665e9f44856f19760ebe
hossain-shifat <hossainshifat222@gmail.com>
2026-06-13 08:29:01 +0600

    Fix: Fix Navbar multiple search component and Searchbar infinite req in backend

--- web/src/pages/home/HomeMediaSections.jsx
+++ web/src/pages/home/HomeMediaSections.jsx
@@ -3,7 +3,7 @@ import MediaRow from "../../Components/MediaRow";
 
 export const HomeMediaSections = ({ movieItems, seriesItems, animeItems, onPlay, onWatchTrailer }) => {
     return (
-        <div className="space-y-15">
+        <div className="space-y-5">
             <div>{movieItems.length > 0 && <MediaRow title="Movies" items={movieItems} onPlay={onPlay} onWatchTrailer={onWatchTrailer} viewAllTo="/movies" />}</div>
             <div>{seriesItems.length > 0 && <MediaRow title="Series" items={seriesItems} onPlay={onPlay} onWatchTrailer={onWatchTrailer} viewAllTo="/series" />}</div>
             <div>{animeItems.length > 0 && <MediaRow title="Anime" items={animeItems} onPlay={onPlay} onWatchTrailer={onWatchTrailer} />}</div>
